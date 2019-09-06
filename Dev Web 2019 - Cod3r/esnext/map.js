77ec2da87161527438523db4b3beb27c9c9738a8
Faran42 <faranorphys@gmail.com>
2019-09-06 16:18:16 -0300

    Aula dia 6 de setembro

--- Dev Web 2019 - Cod3r/esnext/map.js
+++ Dev Web 2019 - Cod3r/esnext/map.js
@@ -9,3 +9,12 @@ const chavesVariadas = new Map([
     [{}, 'Objeto'],
     [123, 'Número'],
 ])
+
+chavesVariadas.forEach((vl, ch) => {
+    console.log(ch, vl)
+})
+
+console.log(chavesVariadas.has(123))
+chavesVariadas.delete(123)
+console.log(chavesVariadas.has(123))
+console.log(chavesVariadas.size)
