f344e8fec558d292d410b49e9ff8e1dfff2d52e1
Faran42 <faranorphys@gmail.com>
2019-11-27 16:18:14 -0300

    First Commit, user crud ok

---
+++ crudTest/project2/server.js
@@ -0,0 +1,25 @@
+require('./models/db')
+
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+
+const userController = require('./controllers/userController');
+
+const app = express()
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('views engine', 'hbs');
+
+
+
+app.listen(3000, function(){
+    console.log('Listen to Port 3000')
+})
+
+app.use('/user', userController);
